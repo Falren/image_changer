f7af8229db254f259c1a744a0621865cc1ff3b2c
Falren <nokuchie@gmail.com>
2023-12-04 00:13:55 +0200

    Test vance image processing

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,34 +1,83 @@
 module Api
   class ImagesController < ApplicationController
-    URL = 'https://www.cutout.pro/api/v1/cartoonSelfie?cartoonType=1'
-    API_KEY = 'b2a93a5c901245ab94c9e1beccac2f57'
+    BASE_URL = 'https://api-service.vanceai.com/web_api/v1/'
+    API_URL = {
+      transform: 'transform',
+      upload: 'upload',
+      download: 'download'
+    }
+    JCONFIG = { sketch: {
+      "job": "sketch",
+      "config": {
+        "module": "sketch",
+        "module_params": {
+          "model_name": "SketchStable",
+          "single_face": true,
+          "composite": true,
+          "sigma": 0,
+          "alpha": 0
+        },
+        "out_params": {}
+      },
+      cartoonize: 
+      {
+          "job": "cartoonize",
+          "config": {
+              "module": "cartoonize",
+              "module_params": {
+                  "model_name": "CartoonizeStable"
+              }
+          }
+      }
+    }}
     
     def create
       return render json: { error: 'No file uploaded' }, status: :unprocessable_entity unless params[:file].present?
 
-      uploaded_file = params[:file]
-      saved_file_path = Rails.root.join('public', 'uploads', uploaded_file.original_filename)      
-      response = send_file_to_external_api(saved_file_path)
-      return render json: { error: 'Error processing the file' }, status: :unprocessable_entity if response.code != 200
-      File.open(saved_file_path, 'wb') do |output_file|
-        output_file.write(response.body)
-        send_file(
-          Rails.root.join('public', 'uploads', uploaded_file.original_filename),
-          filename: uploaded_file.original_filename,
-          type: uploaded_file.content_type,
-          disposition: 'attachment'
-        )
+      saved_file_path = Rails.root.join('public', 'uploads', uploaded_file.original_filename)
+      image = File.open(saved_file_path, 'w') do |file|
+        file.puts process_image(:cartoonize, params[:file]) 
       end
+      image = File.open(saved_file_path, 'w') do |file|
+        file.puts process_image(:sketch, image) 
+      end
+
+      send_file(
+        image,
+        filename: params[:file].original_filename,
+        type: params[:file].content_type,
+        disposition: 'attachment'
+      )
     end
 
     private 
-    
-    def send_file_to_external_api(file_path)
-      RestClient.post(
-        URL,
-        { file: File.new(file_path) },
-        { APIKEY: API_KEY }
+
+    def process_image(type, file)
+      response = RestClient.post(
+        BASE_URL + API_URL[:upload],
+        {
+          api_token: ENV['VANCE_API_KEY'],
+          file: file
+        }
       )
+      uid = JSON.parse(response.body)['data']['uid']
+      response = RestClient.post(
+        BASE_URL + API_URL[:transform],
+        {
+          api_token: ENV['VANCE_API_KEY'],
+          uid: uid,
+          jconfig: JCONFIG[:sketch].to_json
+        }
+      )
+      trans_id = JSON.parse(response.body)['data']['trans_id']
+      response = RestClient.post(
+        BASE_URL + API_URL[:download],
+        {
+          api_token: ENV['VANCE_API_KEY'],
+          trans_id: trans_id
+        }
+      )
+      response.body
     end
   end
 end

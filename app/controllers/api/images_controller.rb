e177ca0d97131aca7d2197bcecefb2d66b836c24
Falren <nokuchie@gmail.com>
2023-12-04 23:28:14 +0200

    add devise jwt

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,10 +1,20 @@
 module Api
   class ImagesController < ApplicationController
+    before_action :authenticate_user!
+    
     def create
       return render json: { error: 'No file uploaded' }, status: :unprocessable_entity unless params[:file].present?
 
       process_image(:cartoonize, params[:file], 'cartoons') 
       render json: { status: :ok }
     end
+  end
+
+  
+  def get_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                             Rails.application.credentials.devise[:jwt_secret_key]).first
+    user_id = jwt_payload['sub']
+    User.find(user_id.to_s)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+              controllers: {
+                sessions: 'users/sessions',
+                registrations: 'users/registrations'
+              }
+
   namespace :api do
     resources :images, only: [:create, :index]
   end
+
   namespace :webhooks do
     resources :cartoons, only: :index
     resources :sketches, only: :index
-  end 
+  end
 end
